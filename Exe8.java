ea7a9cbad8812f9fbec7ce7b501f6822e7687f69
fernandotavaresperpetuo <tavaresfernando.perpetuo@gmail.com>
2021-09-21 13:04:26 -0300

    adicionei

---
+++ Exe8.java
@@ -0,0 +1,13 @@
+public class Exe8 {
+    public static void main(String[] args) {
+        int n = 8;
+        if (n % 1 == 0)
+        {
+            System.out.println(n +" eh um numero primo");
+        }
+        else
+            {
+            System.out.println(n +" nao eh um numero primo");
+            }
+    }
+}
---
+++ exe7.java
@@ -0,0 +1,36 @@
+public class exe7 {
+    public static void main(String[] args){
+        int t = 1;
+        System.out.println(t);
+        int te = 2;
+        System.out.println(te);
+        double tes = 10;
+        System.out.println(tes);
+        double test = 20;
+        System.out.println(test);
+        float teste = 1.1f;
+        System.out.println(teste);
+        float teste1 = 1.2f;
+        System.out.println(teste1);
+        boolean teste2 = true;
+        System.out.println(teste2);
+        boolean teste3 = false;
+        System.out.println(teste3);
+        char teste4 = 'a';
+        System.out.println(teste4);
+        char teste5 = 'b';
+        System.out.println(teste5);
+        byte teste6 = 126;
+        System.out.println(teste6);
+        byte teste7 = 127;
+        System.out.println(teste7);
+        short teste8 = 32766;
+        System.out.println(teste8);
+        short teste9 = 32767;
+        System.out.println(teste9);
+        long teste10 = 9223372036854775807L;
+        System.out.println(teste10);
+        long teste11 = 8223372036854775807L;
+        System.out.println(teste11);
+    }
+}
